e0b5f91119388aa0e583d09a1d93319e05a9b28c
Jim <jim@pop-os.localdomain>
2025-05-07 22:10:56 -0500

    Start attempt at usingg AI

---
+++ new.c
@@ -0,0 +1,3 @@
+#include "new.h"
+
+// Function implementations
---
+++ new.h
@@ -0,0 +1,13 @@
+#ifndef NEW_H
+#define NEW_H
+
+// Include standard libraries
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Type definitions
+
+// Function declarations
+
+#endif // NEW_H
